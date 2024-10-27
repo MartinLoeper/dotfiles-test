d1c1ad6f15d17434642029eff7a0cb5769f6af99
Martin Löper <martin.loeper@nesto-software.de>
2024-10-28 00:16:04 +0100

    fix: paths

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NESTO_DOTFILES_DEST_DIR="${SCRIPT_DIR}"
 
 # WORKAROUND: running dotfiles as non-root throws permission errors when trying to access coder binary
 # if current user is root, rerun the script as user vscode
 if [ "$EUID" -eq 0 ]; then
     # copy the current dir to the vscode user home
-    install -d /home/vscode/dotfiles
-    cp -r "${SCRIPT_DIR}/"* /home/vscode/dotfiles
-    chown -R vscode:vscode /home/vscode/dotfiles
+    NESTO_DOTFILES_DEST_DIR=/home/vscode/dotfiles
+    install -d "${NESTO_DOTFILES_DEST_DIR}"
+    cp -r "${SCRIPT_DIR}/"* "${NESTO_DOTFILES_DEST_DIR}"
+    chown -R vscode:vscode "${NESTO_DOTFILES_DEST_DIR}"
 
-    su - vscode -c "bash /home/vscode/dotfiles/install.sh"
+    su - vscode -c "bash ${NESTO_DOTFILES_DEST_DIR}/install.sh"
     exit
 fi
 
-./git/set-editor-to-nano
+"${NESTO_DOTFILES_DEST_DIR}/git/set-editor-to-nano"
